38d454343b99c9b2dd8a8e14f91cbcf06e6473ce
User <addisumm@gmail.com>
2022-02-21 11:08:09 -0600

    Chapter3 - Branch14: @preAuthorize annotation in place of antMatchers at HttpSecurity

--- demo/demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ demo/demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,6 +19,7 @@ import static com.example.demo.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
@@ -34,10 +36,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests ()
                 .antMatchers ("/", "index", "/css/x", "/js/x").permitAll ()
                 .antMatchers ("/api/**").hasRole (STUDENT.name ()) // This helps to implement role based access
-                .antMatchers (HttpMethod.DELETE,"/management/api/**").hasAuthority (COURSE_WRITE.getPermission ())
-                .antMatchers (HttpMethod.POST,"/management/api/**").hasAuthority (COURSE_WRITE.getPermission ())
-                .antMatchers (HttpMethod.PUT,"/management/api/**").hasAuthority (COURSE_WRITE.getPermission ())
-                .antMatchers (HttpMethod.GET,"/management/api/**").hasAnyRole (ADMIN.name (), ADMINTRINEE.name ())
+//                .antMatchers (HttpMethod.DELETE,"/management/api/**").hasAuthority (COURSE_WRITE.getPermission ())
+//                .antMatchers (HttpMethod.POST,"/management/api/**").hasAuthority (COURSE_WRITE.getPermission ())
+//                .antMatchers (HttpMethod.PUT,"/management/api/**").hasAuthority (COURSE_WRITE.getPermission ())
+//                .antMatchers (HttpMethod.GET,"/management/api/**").hasAnyRole (ADMIN.name (), ADMINTRINEE.name ())
                 .anyRequest ()
                 .authenticated ()
                 .and ()
